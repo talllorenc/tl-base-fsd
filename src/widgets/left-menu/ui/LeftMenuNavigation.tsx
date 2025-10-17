eb721281435963312e812999adc31a8eb800bac5
Alexandr <talllorenc@gmail.com>
2025-10-17 17:47:32 +0500

    Маленькие правки

--- src/widgets/left-menu/ui/LeftMenuNavigation.tsx
+++ src/widgets/left-menu/ui/LeftMenuNavigation.tsx
@@ -9,18 +9,18 @@ const LeftMenuNavigation = () => {
       <p className="uppercase text-sm font-semibold text-foregroundSecondary px-4">
         navigation
       </p>
-      <nav className="flex flex-col gap-2 w-full mt-2">
+      <nav className="flex flex-col w-full mt-2">
         {leftMenuItems.map((link) => {
           const Icon = link.icon;
           return (
             <Link
               key={link.id}
               href={link.href}
-              className="flex items-center justify-between gap-2 px-4 py-2 border border-background rounded-xl hover:bg-backgroundSecondary hover:border-outline transition"
+              className="flex items-center justify-between px-4 py-2 border border-background rounded-xl hover:bg-backgroundSecondary hover:border-outline transition"
             >
               <div className="flex items-center gap-2">
                 {Icon && <Icon className="w-5 h-5 text-muted-foreground" />}
-                <span className="text-sm font-medium">{link.title}</span>
+                <span className="font-medium">{link.title}</span>
               </div>
               <ChevronRight className="w-5 h-5 text-foreground" />
             </Link>
